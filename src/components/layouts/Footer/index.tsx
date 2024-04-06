3ebfacb0bc7b71e4f84c79d5e53bd2f19b829f74
Pili Hu <e@hupili.net>
2024-04-06 11:40:22 +0800

    add Footer component

---
+++ src/components/layouts/Footer/index.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return <>
+        <hr />
+        <p>Place holder of footer</p>
+        <p><a href="https://github.com/irunart/photocast">GitHub</a></p>
+    </>
+}
+
+export default Footer
