2e1a842662967e95d8280ab909b512d6a24e2d28
lanyt <lanyutao@youzan.com>
2024-04-08 22:55:38 +0800

    feat: init version 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import postcsspxtoviewport from "postcss-px-to-viewport";
+
+// import postcsspxtoviewport from "postcss-px-to-viewport";
 // import proxy from './proxy';
 import path from "path";
 
