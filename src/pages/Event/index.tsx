c8ed4c67a74824e58d60421297d02ee3a8ea225b
lanyt <lanyutao@youzan.com>
2024-04-08 23:01:32 +0800

    feat: image swipe to image

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Input, Flex } from "antd";
 import { Image, ImageViewer, Popup, CheckList, CascadePicker, Grid, Card } from "antd-mobile";
@@ -15,19 +15,23 @@ const Home = () => {
 	// const location = useLocation();
 	const navigate = useNavigate();
 	const { event } = useParams();
+	const imageViewerRefs = useRef();
 
 	// 摄影师列表
 	const [photoGraphers, setPhotoGraphers] = useState([]);
 	// 当前摄影师
 	const [grapher, setGrapher] = useState(null);
-
+	// 选择摄影师弹窗
 	const [grapherPopupVisible, setGraperPopupVisible] = useState(false)
 
+	// 照片列表
 	const [images, setImages] = useState([]);
 
-
+	// 当前[日期, 小时] 
 	const [currentDateTime, setCurrentDateTime] = useState([]);
+	// 选择日期时间弹窗
 	const [dateTimePopVisible, setDateTimePopVisible] = useState(false);
+	//照片弹窗
 	const [imagePopVisible, setImagePopVisible] = useState(false)
 
 	if (!event) navigate("/home", { replace: true });
@@ -60,6 +64,11 @@ const Home = () => {
 		setCurrentDateTime([dates[0], hours[0]])
 	}
 
+	const openImageViewer = index => {
+		setImagePopVisible(true)
+		imageViewerRefs.current.swipeTo(index);
+	}
+
 	useEffect(() => {
 		if (!grapher?.value) return;
 		getPhotoDateHourData(grapher?.value, currentDateTime[0], currentDateTime[1]).then(datas => {
@@ -79,7 +88,7 @@ const Home = () => {
 			<p></p>
 			<Grid columns={2} gap={8} >
 				{images.map((image, index) => (
-					<Grid.Item key={image.name} onClick={() => setImagePopVisible(true)}>
+					<Grid.Item key={image.name} onClick={() => openImageViewer(index)}>
 						<Image
 							lazy={true}
 							src={image?.url}
@@ -128,6 +137,7 @@ const Home = () => {
 				onConfirm={(val) => setCurrentDateTime(val)}
 			/>
 			<ImageViewer.Multi
+				ref={imageViewerRefs}
 				images={images.map(i => i.url)}
 				visible={imagePopVisible}
 				defaultIndex={1}
