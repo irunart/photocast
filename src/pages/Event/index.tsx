1aeafee5bb4390a4e0af38ce9466f38204f600cb
yutao.lan <yutao.lan@lachesis-mh.com>
2024-04-09 09:42:25 +0800

    fixed: 懒加载

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -2,8 +2,20 @@ import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { Input, Flex } from "antd";
-import { Image, ImageViewer, Popup, CheckList, CascadePicker, Grid, Card } from "antd-mobile";
-import { DownOutlined, MehOutlined, ClockCircleOutlined } from '@ant-design/icons'
+import {
+	Image,
+	ImageViewer,
+	Popup,
+	CheckList,
+	CascadePicker,
+	Grid,
+	Card,
+} from "antd-mobile";
+import {
+	DownOutlined,
+	MehOutlined,
+	ClockCircleOutlined,
+} from "@ant-design/icons";
 
 import { getPhotographers, getPhotoDateHourData } from "@/services/googleApis";
 import { getEventPhotoGrapher, grapherDateToCascadeOptions } from "./common";
@@ -23,19 +35,17 @@ const Home = () => {
 	// 当前摄影师
 	const [grapher, setGrapher] = useState(null);
 	// 选择摄影师弹窗
-	const [grapherPopupVisible, setGraperPopupVisible] = useState(false)
+	const [grapherPopupVisible, setGraperPopupVisible] = useState(false);
 
 	// 照片列表
 	const [images, setImages] = useState([]);
 
-	// 当前[日期, 小时] 
+	// 当前[日期, 小时]
 	const [currentDateTime, setCurrentDateTime] = useState([]);
 	// 选择日期时间弹窗
 	const [dateTimePopVisible, setDateTimePopVisible] = useState(false);
 	//照片弹窗
-	const [imagePopVisible, setImagePopVisible] = useState(false)
-
-
+	const [imagePopVisible, setImagePopVisible] = useState(false);
 
 	useEffect(() => {
 		if (!event) return navigate("/home", { replace: true });
@@ -44,7 +54,7 @@ const Home = () => {
 		getPhotographers().then((res) => {
 			const grapherLists = getEventPhotoGrapher(res, event);
 			// setGrapher(grapherLists[0]);
-			initSetGrapher(grapherLists[0])
+			initSetGrapher(grapherLists[0]);
 			setPhotoGraphers(grapherLists);
 		});
 	}, [event]);
@@ -53,51 +63,67 @@ const Home = () => {
 		const selectedGrapher = photoGraphers.find(
 			(item) => item.value === nextGrapher
 		);
-		initSetGrapher(selectedGrapher)
+		initSetGrapher(selectedGrapher);
 	};
 
 	// 选择摄影师 ，自动选择日期和时间
-	const initSetGrapher = grapher => {
-		const dates = Object.keys(grapher.available_time)
-		const hours = grapher.available_time[dates[0]]
+	const initSetGrapher = (grapher) => {
+		const dates = Object.keys(grapher.available_time);
+		const hours = grapher.available_time[dates[0]];
 
 		setGrapher(grapher);
-		setCurrentDateTime([dates[0], hours[0]])
-	}
+		setCurrentDateTime([dates[0], hours[0]]);
+	};
 
-	const openImageViewer = index => {
-		setImagePopVisible(true)
+	const openImageViewer = (index) => {
+		setImagePopVisible(true);
 		imageViewerRefs.current.swipeTo(index);
-	}
+	};
 
 	useEffect(() => {
 		if (!grapher?.value) return;
-		getPhotoDateHourData(grapher?.value, currentDateTime[0], currentDateTime[1]).then(datas => {
-			setImages(datas)
-			console.log('datas', datas)
-		})
-
-	}, [currentDateTime])
-
+		getPhotoDateHourData(
+			grapher?.value,
+			currentDateTime[0],
+			currentDateTime[1]
+		).then((datas) => {
+			setImages(datas);
+			console.log("datas", datas);
+		});
+	}, [currentDateTime]);
 
 	return (
 		<div>
 			<p>current photographer below:</p>
-			<Input prefix={<MehOutlined />} value={grapher?.label} onClick={() => setGraperPopupVisible(true)} placeholder="Select a photographer" readOnly suffix={<DownOutlined />} />
+			<Input
+				prefix={<MehOutlined />}
+				value={grapher?.label}
+				onClick={() => setGraperPopupVisible(true)}
+				placeholder="Select a photographer"
+				readOnly
+				suffix={<DownOutlined />}
+			/>
 			<p>current photo date time:</p>
-			<Input prefix={<ClockCircleOutlined />} value={currentDateTime.join('-')} onClick={() => setDateTimePopVisible(true)} placeholder="Select date and time" readOnly suffix={<DownOutlined />} />
+			<Input
+				prefix={<ClockCircleOutlined />}
+				value={currentDateTime.join("-")}
+				onClick={() => setDateTimePopVisible(true)}
+				placeholder="Select date and time"
+				readOnly
+				suffix={<DownOutlined />}
+			/>
 			<p></p>
 
-			<Grid columns={2} gap={8} >
+			<Grid columns={2} gap={8}>
 				{images.map((image, index) => (
 					<Grid.Item key={image.name} onClick={() => openImageViewer(index)}>
 						<Image
+							height="300px" // lazy 需要
 							lazy
 							src={image?.url}
-							fit='cover'
+							fit="cover"
 							style={{ borderRadius: 4 }}
 						/>
-
 					</Grid.Item>
 				))}
 			</Grid>
@@ -106,22 +132,26 @@ const Home = () => {
 				onMaskClick={() => setGraperPopupVisible(false)}
 				destroyOnClose
 			>
-				<p className="text-center">   Select a photographer below:</p>
+				<p className="text-center"> Select a photographer below:</p>
 				<CheckList
 					defaultValue={grapher?.value ? [grapher.value] : []}
-					onChange={val => {
-						handlePhotoGrapherChange(val[0])
-						setGraperPopupVisible(false)
+					onChange={(val) => {
+						handlePhotoGrapherChange(val[0]);
+						setGraperPopupVisible(false);
 					}}
 				>
-					{photoGraphers.map(item => (
-						<CheckList.Item key={item?.value} value={item?.value} className={styles.photoGraperItem}>
+					{photoGraphers.map((item) => (
+						<CheckList.Item
+							key={item?.value}
+							value={item?.value}
+							className={styles.photoGraperItem}
+						>
 							<Flex align="center">
 								<Image
 									src={item?.photographer_icon_url}
 									width={40}
 									height={40}
-									fit='cover'
+									fit="cover"
 									style={{ borderRadius: 4 }}
 								/>
 								<span>&nbsp;{item?.label}</span>
@@ -131,7 +161,7 @@ const Home = () => {
 				</CheckList>
 			</Popup>
 			<CascadePicker
-				title='select date time'
+				title="select date time"
 				options={grapherDateToCascadeOptions(grapher)}
 				visible={dateTimePopVisible}
 				onClose={() => setDateTimePopVisible(false)}
@@ -139,7 +169,7 @@ const Home = () => {
 			/>
 			<ImageViewer.Multi
 				ref={imageViewerRefs}
-				images={images.map(i => i.url)}
+				images={images.map((i) => i.url)}
 				visible={imagePopVisible}
 				defaultIndex={1}
 				onClose={() => setImagePopVisible(false)}
