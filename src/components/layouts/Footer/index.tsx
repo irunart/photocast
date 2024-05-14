8803a0444628c3189d0f2d11fc1833d6a1656a80
yutao.lan <goby.hl@gmail.com>
2024-05-14 11:34:05 +0800

    feat: 增加建议

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -1,9 +1,12 @@
-import { FloatButton, message } from "antd";
-import { ShareAltOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { FloatButton, message, Modal } from "antd";
+import { ShareAltOutlined, CommentOutlined } from "@ant-design/icons";
 import styles from "./index.module.scss";
 
 const Footer: React.FC = () => {
   const [messageApi, contextHolder] = message.useMessage();
+  const [modalVisible, setModalVisible] = useState(false);
+
   const handleShareBtnClick = () => {
     navigator.clipboard.writeText(window.location.href).then(
       () => {
@@ -17,17 +20,44 @@ const Footer: React.FC = () => {
     );
   };
 
+  const openForm = () => {
+    setModalVisible(true); // Open the modal when the form button is clicked
+  };
+
+  const closeModal = () => {
+    setModalVisible(false); // Close the modal
+  };
+
   return (
-    <div className={styles.footer}>
-      {contextHolder}
-      <p className={styles.github}>
-        <a href="https://github.com/irunart/photocast">GitHub.com</a>
-      </p>
-      <FloatButton.Group>
-        <FloatButton icon={<ShareAltOutlined />} onClick={handleShareBtnClick} />
-        <FloatButton.BackTop />
-      </FloatButton.Group>
-    </div>
+    <>
+      <Modal
+        title="Feedback Form"
+        open={modalVisible}
+        onCancel={closeModal}
+        footer={null} // Remove the default footer buttons
+        width={640} // Set the width of the modal
+      >
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdwCyjuEJt9TUUNDnRzUuJ8wYbHg9RHNQwaCKxbNu9QeCaO3A/viewform?embedded=true"
+          width="100%"
+          height="400"
+          frameborder="0"
+        >
+          正在加载…
+        </iframe>
+      </Modal>
+      <div className={styles.footer}>
+        {contextHolder}
+        <p className={styles.github}>
+          <a href="https://github.com/irunart/photocast">GitHub.com</a>
+        </p>
+        <FloatButton.Group>
+          <FloatButton icon={<ShareAltOutlined />} onClick={handleShareBtnClick} />
+          <FloatButton icon={<CommentOutlined />} onClick={openForm} />
+          <FloatButton.BackTop />
+        </FloatButton.Group>
+      </div>
+    </>
   );
 };
 
