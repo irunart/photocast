91b031138080eb13c9c559a533b7978d9b72789f
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-07 10:57:58 +0800

    Match list layout optimization: 1) List to grid

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import * as _ from "lodash-es";
 
-import { List, Image } from "antd-mobile";
+import { Grid, Card, Image } from "antd-mobile";
 
 import { getEventLists } from "@/services/googleApis";
 
@@ -13,43 +13,32 @@ const Home = () => {
 
   const [data, setData] = useState<string[]>([]);
 
-  const datasRef = useRef<IEventLists>();
+  const dataRef = useRef<IEventLists>();
 
   // init 阶段
   useEffect(() => {
     getEventLists().then((res: CommonResponse<IEventLists>) => {
-      datasRef.current = res.data;
+      dataRef.current = res.data;
       setData(_.keys(res.data));
     });
   }, []);
 
   const goEventDetail = (name: string) => {
-    navigate("/event/" + name, { state: { data: datasRef.current?.[name] } });
+    navigate("/event/" + name, { state: { data: dataRef.current?.[name] } });
   };
 
   //TODO: api接口 赛事详情,对应图片
 
   return (
-    <List>
+    <Grid columns={3} gap={8}>
       {data.map((event) => (
-        <List.Item
-          prefix={
-            <Image
-              src="https://iest.run/IEST-flag.jpg"
-              style={{ borderRadius: 20 }}
-              fit="cover"
-              width={40}
-              height={40}
-            />
-          }
-          description={event + "赛事"}
-          key={event}
-          onClick={() => goEventDetail(event)}
-        >
-          {event}
-        </List.Item>
+        <Grid.Item onClick={() => goEventDetail(event)}>
+          <Card title={event + "赛事"}>
+            <Image src="https://iest.run/IEST-flag.jpg" style={{ borderRadius: 20 }} fit="cover" />
+          </Card>
+        </Grid.Item>
       ))}
-    </List>
+    </Grid>
   );
 };
 
