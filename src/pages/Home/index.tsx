6e05eb019ac2ef60347cf3ad21df37a6603b92e0
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-07 12:53:57 +0800

    event sortBy  startdate

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import * as _ from "lodash-es";
+// import { sortBy } from 'lodash-es';
 
 import { Grid, Card, Image } from "antd-mobile";
 
-import { getEventLists } from "@/services/googleApis";
+// import { getEventLists } from "@/services/googleApis";
+import { getEventDetail } from "@/services/googleApis";
 
 import type { IEventLists } from "./type";
 
@@ -17,8 +19,11 @@ const Home = () => {
 
   // init 阶段
   useEffect(() => {
-    getEventLists().then((res: CommonResponse<IEventLists>) => {
-      dataRef.current = res.data;
+    getEventDetail().then((res: CommonResponse<IEventLists>) => {
+      _.sortBy(res.data, ["date_start"], ["asc"]);
+
+      // dataRef.current.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
       setData(_.keys(res.data));
     });
   }, []);
@@ -28,11 +33,10 @@ const Home = () => {
   };
 
   //TODO: api接口 赛事详情,对应图片
-
   return (
     <Grid columns={3} gap={8}>
       {data.map((event) => (
-        <Grid.Item onClick={() => goEventDetail(event)}>
+        <Grid.Item onClick={() => goEventDetail(event)} key={event}>
           <Card title={event + "赛事"}>
             <Image src="https://iest.run/IEST-flag.jpg" style={{ borderRadius: 20 }} fit="cover" />
           </Card>
--- src/pages/Home/type.ts
+++ src/pages/Home/type.ts
@@ -2,7 +2,15 @@ export interface IEventLists {
   [event: string]: IEventDetail[];
 }
 interface IEventDetail {
-  label: string;
-  evnet: string;
+  // label: string;
+  event: string;
   name: string;
+  category: string;
+  country: string;
+  city: string;
+  location: string;
+  date_start: string;
+  date_end: string;
+  event_icon_url: string;
+  website: string;
 }
