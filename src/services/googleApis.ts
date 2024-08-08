1aa6073d780fedaa3d23a6278640fc351343c5bb
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-08 12:27:25 +0800

    delete code about "events.json"

--- src/services/googleApis.ts
+++ src/services/googleApis.ts
@@ -1,14 +1,11 @@
 import request from "@/utils/request";
 
 const urlPhotographers = "/photographers.json";
-const urlEvents = "/events.json";
 const urlEventDateRename = "/event_date_rename.json";
 const urlEventDetail = "/event_detail.json";
 const urlDefault = "/defaults.json";
 
 const withNounce = (url: string) => `${url}?${Math.random()}`;
-
-export const getEventLists = () => request.get(withNounce(urlEvents));
 
 export const getPhotographers = () => request.get(withNounce(urlPhotographers));
 
