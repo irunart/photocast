3126d110c7a3289a5252cd3dc886167574a13588
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-08 12:37:53 +0800

    delete code about "defaults.json"

--- src/services/googleApis.ts
+++ src/services/googleApis.ts
@@ -3,7 +3,6 @@ import request from "@/utils/request";
 const urlPhotographers = "/photographers.json";
 const urlEventDateRename = "/event_date_rename.json";
 const urlEventDetail = "/event_detail.json";
-const urlDefault = "/defaults.json";
 
 const withNounce = (url: string) => `${url}?${Math.random()}`;
 
@@ -12,9 +11,6 @@ export const getPhotographers = () => request.get(withNounce(urlPhotographers));
 export const getEventDateRename = () => request.get(withNounce(urlEventDateRename));
 
 export const getEventDetail = () => request.get(withNounce(urlEventDetail));
-
-// 默认 event
-export const getDefault = () => request.get(withNounce(urlDefault));
 
 export const getPhotoDateHourData = (photoGrapher: string, date: string, hour: string) =>
   request.get(withNounce(`/${photoGrapher}/${date}/${hour}.json`));
