baad879d2dcd0ddcdb2c370e7a965eb198ea5b49
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-13 02:36:42 +0800

    uodate Disclaimer

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -12,14 +12,14 @@ const About = () => {
       </p>
       <p>
         本網頁僅為參賽者分享圖片內容提供網路平台與儲存空間，所分享之圖片將不時進行增減或更新。
-        如有需要下載個人的完整圖片文件，或希望從本網頁移除，請與我們聯絡: hi@iest.run
+        如有需要下載個人的完整圖片文件，或希望從本網頁移除，請與我們聯絡: <b>hi@iest.run</b>.
       </p>
 
       <p>
         {" "}
         Photocast is a platform to provide storage of pictures for trail runners. Photocase might add, revmove or update
         the pictures from time to time. For donwloading original photo, or removing your pictures from Photocast, please
-        contact us: hi@iest.run .
+        contact us: <b>hi@iest.run</b> .
       </p>
     </>
   );
