507edde22d51d70442a68b96a36deca5ca0364f3
Huaaji <87155801+HuaaJi@users.noreply.github.com>
2024-08-18 00:55:14 +0800

    header style

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
 import styles from "./index.module.scss";
-import { Image } from "antd-mobile";
+import { Image, Grid } from "antd-mobile";
+
 const Header = () => {
   return (
     <>
-      <Link to="/">
-        <h1>PhotoCast</h1>
-      </Link>
+      <Grid columns={2}>
+        <Grid.Item>
+          <Link to="/">
+            <h1>PhotoCast</h1>
+          </Link>
 
-      <em className={styles.summary}>
-        {"A runners-for-runners photo system by "}
-        <a href="https://iest.run">iest.run</a>.
-      </em>
-      <Link to="https://iest.run/" className={styles.title} style={{ paddingBottom: "10px" }}>
-        <Image src="https://iest.run/assets/img/IEST-logo-with-text-horizontal.png" width={"200px"}></Image>
-      </Link>
+          <em className={styles.summary}>
+            {"A runners-for-runners photo system by "}
+            <a href="https://iest.run">iest.run</a>.
+          </em>
+        </Grid.Item>
+        <Grid.Item>
+          <Link to="https://iest.run/" className={styles.title} style={{ paddingBottom: "10px" }}>
+            <Image
+              src="https://iest.run/assets/img/IEST-logo-with-text-horizontal.png"
+              width={"150px"}
+              style={{ position: "absolute", right: 0 }}
+            ></Image>
+          </Link>
+        </Grid.Item>
+      </Grid>
     </>
   );
 };
