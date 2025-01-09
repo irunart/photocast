d17d5bb70c023460a41c1794e44c429dc2f1832e
yutao.lan <goby.hl@gmail.com>
2025-01-09 16:05:44 +0800

    fixed mobile antd i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,45 +3,59 @@ import ReactDOM from "react-dom/client";
 import { useTranslation } from "react-i18next";
 import { StyleProvider, legacyLogicalPropertiesTransformer } from "@ant-design/cssinjs";
 import App from "@/routes";
-
 import { ConfigProvider } from "antd";
-import zhCN from "antd/locale/zh_CN";
-import enUS from "antd/locale/en_US";
-import zhTW from "antd/locale/zh_TW";
-
+import { ConfigProvider as ConfigProviderMobile } from "antd-mobile";
 import NiceModal from "@ebay/nice-modal-react";
 import dayjs from "dayjs";
+
 import "dayjs/locale/zh-tw"; // 繁体中文
 import "dayjs/locale/zh-cn"; // 简体中文
 import "dayjs/locale/en";
+import zhCN from "antd/locale/zh_CN";
+import enUS from "antd/locale/en_US";
+import zhTW from "antd/locale/zh_TW";
+import en_US from "antd-mobile/es/locales/en-US";
+import zh_CN from "antd-mobile/es/locales/zh-CN";
+import zh_TW from "antd-mobile/es/locales/zh-TW";
+
 import "./i18n";
 
 import "./assets/styles/base.css";
 
-const getAntdLocale = (lng: string) => {
+const getLocale = (lng: string) => {
   switch (lng) {
     case "zh-TW":
       dayjs.locale("zh-tw");
-      return zhTW;
+      return {
+        antd: zhTW,
+        antdMobile: zh_TW,
+      };
     case "zh":
       dayjs.locale("zh-cn");
-      return zhCN;
+      return {
+        antd: zhCN,
+        antdMobile: zh_CN,
+      };
     case "en":
     default:
       dayjs.locale("en");
-      return enUS;
+      return {
+        antd: enUS,
+        antdMobile: en_US,
+      };
   }
 };
 
 // 创建一个包装组件来使用 hooks
 const AppWrapper = () => {
   const { i18n } = useTranslation();
+  const locale = getLocale(i18n.language);
 
   return (
     <React.StrictMode>
       <StyleProvider hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
         <ConfigProvider
-          locale={getAntdLocale(i18n.language)}
+          locale={locale.antd}
           theme={{
             components: {
               FloatButton: {
@@ -50,9 +64,11 @@ const AppWrapper = () => {
             },
           }}
         >
-          <NiceModal.Provider>
-            <App />
-          </NiceModal.Provider>
+          <ConfigProviderMobile locale={locale.antdMobile}>
+            <NiceModal.Provider>
+              <App />
+            </NiceModal.Provider>
+          </ConfigProviderMobile>
         </ConfigProvider>
       </StyleProvider>
     </React.StrictMode>
